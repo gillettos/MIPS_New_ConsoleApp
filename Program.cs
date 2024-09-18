d0716b52675f42ce9e262dd0473845d6bbd6f8fe
Richard Gillett <gillettos1981@hotmail.com>
2024-09-18 15:20:35 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyLibrary;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            string message = myClass.SayHello("World");
+            Console.WriteLine(message);
+        }
+    }
+}
